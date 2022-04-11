7964361c50224319cb4088f29845ab334dbdd6d7
pjpure <pure254326@gmail.com>
2022-04-11 22:42:43 +0700

    test scrap match tenz and shazam

---
+++ match.py
@@ -0,0 +1,122 @@
+import requests
+import re
+import time
+import json
+
+baseURL = "https://liquipedia.net"
+
+
+def getMatchsPlayer(player):
+    matchURL = baseURL+"/valorant/"+player+"/Matches"
+    r = requests.get(matchURL)
+    content = r.text
+    matchs = re.findall(
+        r'<tr style.*?2022-.*?</tr>|<tr style.*?2021-.*?</tr>', content)
+    '''
+    {
+        "player": "",
+        "date": "",
+        "tournament": "",
+        "map": "",
+        "kill": "",
+        "death": "",
+        "assist": "",
+        "team1": "",
+        "team2": "",
+        "agents1": [],
+        "agents2": [],
+        "score": "",
+    }
+    '''
+    regex = {
+        "date": r'\d\d\d\d-\d\d-\d\d',
+        "tournament": r'<td.*?<td.*?<td.*?<td.*?<td>.*?>(.*?)<',
+        "map": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?>.*?>(.*?)<',
+        "kill": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?>(.*?)<',
+        "death": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?>(.*?)<',
+        "assist": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?>(.*?)<',
+        "team1": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?title="(.*?)"',
+        "team2": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?title="(.*?)"',
+        "score": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td>(.*?)</td>',
+        "a1": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td>.*?<span.*?<a.*?title="(.*?)"',
+        "a2": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?title="(.*?)">',
+        "a3": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?<a.*?title="(.*?)">',
+        "a4": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?<a.*?<a.*?title="(.*?)">',
+        "a5": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?<a.*?<a.*?<a.*?title="(.*?)">',
+        "a6": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?title="(.*?)">',
+        "a7": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?<a.*?title="(.*?)">',
+        "a8": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?<a.*?<a.*?title="(.*?)">',
+        "a9": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?<a.*?<a.*?<a.*?title="(.*?)">',
+        "a10": r'<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td.*?<td><a.*?<a.*?<a.*?<a.*?<a.*?title="(.*?)">',
+    }
+    allMatchs = []
+    for match in matchs:
+        date = re.findall(regex["date"], match)
+        tournament = re.findall(regex["tournament"], match)
+        map1 = re.findall(regex["map"], match)
+        kill = re.findall(regex["kill"], match)
+        death = re.findall(regex["death"], match)
+        assist = re.findall(regex["assist"], match)
+        team1 = re.findall(regex["team1"], match)
+        team2 = re.findall(regex["team2"], match)
+        score = re.findall(regex["score"], match)
+        a1 = re.findall(regex["a1"], match)
+        a2 = re.findall(regex["a2"], match)
+        a3 = re.findall(regex["a3"], match)
+        a4 = re.findall(regex["a4"], match)
+        a5 = re.findall(regex["a5"], match)
+        a6 = re.findall(regex["a6"], match)
+        a7 = re.findall(regex["a7"], match)
+        a8 = re.findall(regex["a8"], match)
+        a9 = re.findall(regex["a9"], match)
+        a10 = re.findall(regex["a10"], match)
+
+        score[0] = score[0].replace("&#160;", "")
+        score[0] = score[0].replace("<b>", "")
+        score[0] = score[0].replace("</b>", "")
+        score[0] = score[0].replace(" ", "")
+
+        a1 = a1[0] if len(a1) > 0 else ""
+        a2 = a2[0] if len(a2) > 0 else ""
+        a3 = a3[0] if len(a3) > 0 else ""
+        a4 = a4[0] if len(a4) > 0 else ""
+        a5 = a5[0] if len(a5) > 0 else ""
+        a6 = a6[0] if len(a6) > 0 else ""
+        a7 = a7[0] if len(a7) > 0 else ""
+        a8 = a8[0] if len(a8) > 0 else ""
+        a9 = a9[0] if len(a9) > 0 else ""
+        a10 = a10[0] if len(a10) > 0 else ""
+
+        agents1 = [a1, a2, a3, a4, a5]
+        agents2 = [a6, a7, a8, a9, a10]
+
+        match = {
+            "player": player,
+            "date": date[0] if len(date) > 0 else "",
+            "tournament": tournament[0] if len(tournament) > 0 else "",
+            "map": map1[0] if len(map1) > 0 else "",
+            "kill": kill[0] if len(kill) > 0 else "",
+            "death": death[0] if len(death) > 0 else "",
+            "assist": assist[0] if len(assist) > 0 else "",
+            "team1": team1[0] if len(team1) > 0 else "",
+            "team2": team2[0] if len(team2) > 0 else "",
+            "agents1": agents1,
+            "agents2": agents2,
+            "score": score[0] if len(score) > 0 else "",
+        }
+        print(match)
+        allMatchs.append(match)
+    return allMatchs
+
+
+def write_json(new_data, filename):
+    with open(filename, mode='r', encoding='utf-8') as j:
+        data = json.load(j)
+    data.extend(new_data)
+    with open(filename, 'w', encoding='utf-8') as j:
+        json.dump(data, j, ensure_ascii=False, indent=4)
+
+
+if __name__ == "__main__":
+    allMatchs = getMatchsPlayer("ShahZaM")
+    write_json(allMatchs, "matchs.json")
